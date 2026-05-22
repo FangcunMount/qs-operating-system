f54c42c79e2e6f585b2ed29fc9a01083e786c706
yshujie <yshujie@163.com>
2026-05-22 19:25:08 +0800

    fix jwtClaims.test.ts 里超长 JWT 字符串改为数组 join('')，max-len 与测试均通过。

--- src/utils/jwtClaims.test.ts
+++ src/utils/jwtClaims.test.ts
@@ -1,9 +1,11 @@
 import { parseJwtClaims, validateJwtClaims } from './jwtClaims'
 
-const fangcunAccessToken =
-  'eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS0xNzc3ODMxMjAwIiwidHlwIjoiSldUIn0.' +
-  'eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwidXNlcl9pZCI6IjEwMDAxIiwidGVuYW50X2lkIjoiZmFuZ2N1biIsInJlYWxtIjoiMSIsImV4cCI6MTc3OTQyOTYyOCwiaWF0IjoxNzc5NDI4NzI4fQ.' +
+const fangcunAccessToken = [
+  'eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS0xNzc3ODMxMjAwIiwidHlwIjoiSldUIn0.',
+  'eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwidXNlcl9pZCI6IjEwMDAxIiwidGVuYW50X2lkIjoiZmFuZ2N1biIs',
+  'InJlYWxtIjoiMSIsImV4cCI6MTc3OTQyOTYyOCwiaWF0IjoxNzc5NDI4NzI4fQ.',
   'sig'
+].join('')
 
 describe('jwtClaims IAM V2', () => {
   it('accepts tenant domain in tenant_id when realm is present', () => {
